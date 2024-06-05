4f57a73ad0a9c713007cfaa70acbf639a7c1ecdb
Alonso <alonsonh94@gmail.com>
2024-06-05 15:39:22 -0600

    Add upvotes put endpoint

--- src/server.js
+++ src/server.js
@@ -1,9 +1,32 @@
 import express from "express";
 
+let articleInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "learn-mongodb",
+    upvotes: 0,
+  },
+];
+
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello!");
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  let article = articleInfo.find((article) => article.name === name);
+  if (article) {
+    article.upvotes += 1;
+  } else {
+    res.send("That article doesn't exist");
+  }
+
+  res.send(`The ${name} article now has ${article.upvotes} upvotes`);
 });
 
 app.listen(8000, () => {
